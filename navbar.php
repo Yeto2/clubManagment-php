915d5c2e2e91d2c1f5c0c968b94bf72e986185b5
Yeto2 <bota.tll2004@gmail.com>
2022-11-10 21:32:29 +0100

    index , navbar and footer section

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-dark bg-gradient" style="--bs-bg-opacity: 0.7">
+    <div class="container">
+    <a class="navbar-logo" href="#"><img width="60px" src="images/logo.png" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+        <div class="navbar-nav ">
+        <a class="nav-link active text-white" aria-current="page" href="#home">Home</a>
+        <a class="nav-link text-white" href="#">Clubs</a>
+        <a class="nav-link text-white" href="#">Contact</a>
+        <a class="nav-link text-white" href="#"><button class="btn btn-primary">Join Club</button></a>
+        </div>
+    </div>
+    </div>
+</nav>
