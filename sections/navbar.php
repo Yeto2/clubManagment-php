93d65eb9d0ea088d8fe9a690b7997f88ab5a8ab5
Yeto2 <bota.tll2004@gmail.com>
2022-11-25 22:43:21 +0100

    login pages

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,17 +1,36 @@
 <nav class="navbar navbar-expand-lg bg-dark bg-gradient" style="--bs-bg-opacity: 0.7">
     <div class="container">
-    <a class="navbar-logo" href="#"><img width="60px" src="images/logo.png" alt="logo"></a>
+    <a class="navbar-logo" href="#"><img width="60px" src="../images/logo.png" alt="logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+
+    <?php
+        if (!isset($_SESSION["email"]) || empty($_SESSION["cne"]))
+        {
+        ?>
     <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
         <div class="navbar-nav ">
         <a class="nav-link active text-white" aria-current="page" href="#home">Home</a>
         <a class="nav-link text-white" href="#clubs">Clubs</a>
         <a class="nav-link text-white" href="#contact">Contact</a>
+        </div>
         <!-- Button trigger modal -->
         <button class="btn btn-success"data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
-    </div>
+    </div>     
+    <?php }else{?>
+            <div class="navbar-nav ">
+
+            <a class="nav-link active text-white" aria-current="page" href="#home">Add club</a>
+            
+            <a class="nav-link text-white" href="#clubs">Add member</a>
+            </div>
+            <form action="../data/logout.php" method="get">
+                        <input class="btn btn-danger" type="submit" value="logout" name="logout">
+                    </form>
+            <?php
+        }
+        ?>
     </div>
     </div>
 
